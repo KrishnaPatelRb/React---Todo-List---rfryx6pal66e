de05b76949febef531eb62ccfa5a58f09ff75e95
KrishnaPatelRb <93961739+KrishnaPatelRb@users.noreply.github.com>
2022-02-24 10:17:58 +0530

    1230

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
 function App() 
 {
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+	
+    const[list, setList] = useState([]);
+    const [input , setInput] = useState("");
+	const [edit,setEdit]=useState(false)
+	const [editinput,setEditInput]=useState("")
+	const [currenteditIndex,setCurrentEditIndex]=useState("")
+	let newArr=list;
+    return (
+    <div id="main">
+        <textarea id="task" value={input} onChange={(event)=>{setInput(event.target.value)}} ></textarea>
+        <button onClick={()=>{if(input){setList([...list,input])}}} id="button">Add</button>
+		{edit?<><textarea value={editinput} onChange={(event)=>{setEditInput(event.target.value)}}></textarea>
+		<button onClick={()=>{if(editinput){newArr.splice(currenteditIndex,1),newArr.splice(currenteditIndex,0,editinput),setList([...newArr]),setEdit(false)}}}>Save</button></>:""}
+		
+		<ul>
+			{list.map((current,index)=>{return <> <li className="list">{current}</li>
+			<button onClick={()=>{newArr.splice(index,1)
+			setList([...newArr])}}>Delete</button>
+			<button onClick={()=>{setEdit(true),setEditInput(list[index]),setCurrentEditIndex(index)}}>Edit</button></>})}
+		</ul>
+    </div>
+    );
 }
 
 
